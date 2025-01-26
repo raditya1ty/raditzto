0793a96edbc8f8294668907a05760d70950a9a93
raditya1ty <alifiansyahaji1106@gmail.com>
2025-01-26 22:34:33 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-// Fungsi untuk membuka sidebar
-function openSidebar() {
-    document.getElementById("sidebar").style.width = "250px";
-    document.getElementById("main-content").style.marginLeft = "250px";
+document.addEventListener("DOMContentLoaded", () => {
+    const content = document.getElementById("content");
+    content.classList.add("visible");
+});
+
+
+
+import React from 'react'
+
+const script = () => {
+  return (
+    <div>
+      <p> halo teman teman</p>
+    </div>
+  )
 }
 
-// Fungsi untuk menutup sidebar
-function closeSidebar() {
-    document.getElementById("sidebar").style.width = "0";
-    document.getElementById("main-content").style.marginLeft = "0";
-}
-
-// Mengambil elemen audio dari HTML
-const audio = document.getElementById("audio");
-
-// Fungsi untuk memainkan audio
-function playAudio() {
-    audio.play();
-}
-
-// Fungsi untuk menghentikan (pause) audio
-function pauseAudio() {
-    audio.pause();
-}
-
-
+export default script
